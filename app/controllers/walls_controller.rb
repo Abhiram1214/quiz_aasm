794498591bf3d940f158d8b66b65f6eac59d38aa
Abhiram1214 <abhiram_cvns@hotmail.com>
2015-09-25 00:20:25 +0530

    Try using multiple if statements in walls controller

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -6,11 +6,11 @@ class WallsController < ApplicationController
     @friend_logic = Friendship.find_by(:from_id=>current_user.id) || Friendship.find_by(:to_id=>current_user.id)
 
   if @friend_logic!=nil
-     @from_posts = Post.find_by(:user_id=>@friend_logic.from_id)
-     @to_posts = Post.find_by(:user_id=>@friend_logic.to_id)
-     @comments = Comment.all
+     @from_posts = Post.where(:user_id=>@friend_logic.from_id)
+     @to_posts = Post.where(:user_id=>@friend_logic.to_id)
+     #@from_comments =
   end
-  
+
   end
 
 end
