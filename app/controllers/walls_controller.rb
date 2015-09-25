ef96bbd458d44554b9e6104d54e44ef5f0e29554
Abhiram1214 <abhiram_cvns@hotmail.com>
2015-09-25 12:09:42 +0530

    Friendship working correctly & wall has posts from all friends...hurraygit add .git add .git add .

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -3,13 +3,35 @@ class WallsController < ApplicationController
   def index
 
 
-    @friend_logic = Friendship.find_by(:from_id=>current_user.id) || Friendship.find_by(:to_id=>current_user.id)
+    @friend_from_logic = Friendship.where(:from_id=>current_user.id)
+    @friend_to_logic = Friendship.where(:to_id=>current_user.id)
 
-  if @friend_logic!=nil
-     @from_posts = Post.where(:user_id=>@friend_logic.from_id)
-     @to_posts = Post.where(:user_id=>@friend_logic.to_id)
-     
+    @friend_from_logic.each do |friend_from|
+      if friend_from.aasm_state == "friend"
+        @from = friend_from.from_id
+        @from_to = friend_from.to_id
+      end
+    end
+
+    @friend_to_logic.each do |friend_to|
+      if friend_to.aasm_state == "friend"
+        @to = friend_to.to_id
+        @to_from = friend_to.from_id
+      end
+    end
+
+  if @friend_from_logic!=nil
+     @from_posts = Post.where(:user_id=>@from)
+     @to_posts = Post.where(:user_id=>@from_to)
+
   end
+
+
+    if @friend_to_logic!=nil
+       @from_posts_friends = Post.where(:user_id=>@to_from)
+       @to_posts_friends = Post.where(:user_id=>@to)
+
+    end
 
   end
 
